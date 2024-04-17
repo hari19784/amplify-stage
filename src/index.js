c171d01e9bbc03874750376ce1e957726ea0eea7
saikiran9087 <130025811+saikiran9087@users.noreply.github.com>
2024-04-17 15:12:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Amplify} from 'aws-amplify';
-import  config from './aws-export'; 
 
-Amplify.configure(config); 
+
+
 reportWebVitals();
  
 const container = document.getElementById('root');
